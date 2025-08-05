5f929fc2f3af2be4a2184169ecd89e53f9982523
snj0625 <snj0625@gmail.com>
2025-08-05 16:02:56 -0400

    updates

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // API: https://www.omdbapi.com/?i=tt3896198&apikey=76fae4ba&s=fast
+
+// This function generates the HTML for each movie card
 function movieHTML(movie) {
-    return `
-    <div class="movie-card">
+    return `<div class="movie-card">
         <img src="${movie.Poster}" alt="${movie.Title}"/>
         <h3>${movie.Title}</h3>
         <p>Year: ${movie.Year}</p>
@@ -9,24 +10,36 @@ function movieHTML(movie) {
     `;
 }
 
+
+// This function fetches movie data from the OMDB API based on the search title
 async function fetchMovieData(searchTitle) {
+
+// Show the loading spinner while fetching data
+const movieListEl = document.querySelector('.movie-list');
+movieListEl.classList.add('loading__icon--spinner'); 
+
+// Fetch movie data from the OMDB API   
 const movies = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=76fae4ba&s=${searchTitle}`);
 const movieData = await movies.json();
-const movieListEl = document.querySelector('.movie-list');
 
+// Hide the loading spinner after fetching data             
+movieListEl.classList.remove('loading__icon--spinner');
+
+
+// If the API returns a list of movies, we will generate the HTML for each movie and display it
 if(movieData.Search){
-
 movieListEl.innerHTML = movieData.Search.slice(0,6)
 .map((movie) => movieHTML(movie)).join("");
 }
-
 else {
     movieListEl.innerHTML = `<p> No movies found</p>`;
 }
-}
 
+}
+//When the user types in the search box, this function will be called
 async function onSearchMovie(event){
 const title = event.target.value;
-fetchMovieData(title);
-}
 
+movies = await fetchMovieData(title);
+
+}
